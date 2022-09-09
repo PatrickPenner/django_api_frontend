d2be05188d41133b4ed84a3dc38998d0dd82e10d
Patrick Penner <patrick@ppenner.com>
2022-09-09 07:40:56 +0200

    custom HTML template rendering

--- custom_renderer/views.py
+++ custom_renderer/views.py
@@ -1,9 +1,19 @@
 from rest_framework import generics
+from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
 
 from .models import Data
 from .serializers import DataSerializer
 
+class TemplateHTMLRendererImplementation(TemplateHTMLRenderer):
+    def get_template_context(self, data, renderer_context):
+        response = renderer_context['response']
+        if response.exception:
+            data['status_code'] = response.status_code
+        return {'data': data}
+
 class DataList(generics.ListAPIView):
     queryset = Data.objects.all()
     serializer_class = DataSerializer
+    renderer_classes = [TemplateHTMLRendererImplementation, JSONRenderer]
+    template_name = 'custom_renderer/data_list.html'
 
